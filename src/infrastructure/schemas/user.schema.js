3282f15082905b9e0d44a8b5de0565fd9415069b
jamescosjr <jamescosjr@gmail.com>
2025-11-06 16:04:39 -0300

    ✨ feat: add user schema

---
+++ src/infrastructure/schemas/user.schema.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { getDoctorByName } from '../repositories/doctor-repositories/doctor.repository.read';
+
+export const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+        select: false
+    },
+    role: {
+        type: String,
+        enum: ['root', 'client', 'doctor', 'employee'],
+        default: 'client',
+        required: true
+    }
+    
+}, { timestamps: true });
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compar (candidatePassword, this.passwordHash);
+};
+
+export const User = mongoose.model('User', userSchema);
